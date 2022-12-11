ca58e89e4e966eabcd1fb9b8f01c8a3ad46065a4
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-11 20:55:11 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,67 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import type { Request, Response } from "express";
+import playwright from "playwright";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(jsonParser);
+app.use(
+  cors({ origin: ["http://localhost:3000", "https://ag-job-sheet.vercel.app"] })
+);
+
+app.get("/", (request: Request, response: Response) => {
+  response.send("Express + TypeScript Server");
+});
+
+app.post("/pdf", async (request: Request, response: Response) => {
+  const { pageRanges, path } = request.body;
+
+  const pdf = request.headers.origin
+    ? await generatePDF({
+        pageRanges,
+        path: `${request.headers.origin}${path}`,
+      })
+    : null;
+
+  return response
+    .status(200)
+    .setHeader("Content-Type", "application/pdf")
+    .send(pdf);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+
+interface IOptions {
+  pageRanges?: string;
+  path: string;
+}
+export const generatePDF = async ({ pageRanges, path }: IOptions) => {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(path, { waitUntil: "networkidle" });
+
+  const pdfGenerator = page.locator(".fixed");
+  await pdfGenerator.evaluate((node) => (node.style.visibility = "hidden"));
+
+  const pdf = await page.pdf({
+    format: "A4",
+    pageRanges,
+    preferCSSPageSize: true,
+    printBackground: true,
+  });
+  await browser.close();
+
+  return pdf;
+};
