e8b0e725ffdfa7ec9019e4430e745345f5ca4b71
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-12 14:04:23 +0800

    misc

--- pdf_generator.ts
+++ pdf_generator.ts
@@ -6,18 +6,20 @@ interface IOptions {
   pageRanges?: string;
   path: string;
 }
+let browser: Browser | null = null;
 export const generatePDF = async ({ pageRanges, path }: IOptions) => {
-  let browser: Browser | null = null;
-
   try {
     const timer: { key: string; time: number }[] = [];
     const start = new Date().getTime();
 
-    browser = await playwright.chromium.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-    });
+    if (!browser) {
+      browser = await playwright.chromium.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+      });
+    }
+
     timer.push({
       key: "browser",
       time: new Date().getTime(),
@@ -54,5 +56,6 @@ export const generatePDF = async ({ pageRanges, path }: IOptions) => {
     console.log(error);
   } finally {
     await browser?.close();
+    browser = null;
   }
 };
