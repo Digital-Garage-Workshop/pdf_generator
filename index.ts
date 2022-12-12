f52622b0f6c8a6b3ada836f1236a6f2c5af4cb3a
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-12 13:03:44 +0800

    logger

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import type { Request, Response } from "express";
+import morgan from "morgan";
 import playwright from "playwright";
 import type { Browser } from "playwright";
 
@@ -13,12 +14,16 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 app.use(jsonParser);
+
 app.use(
   cors({ origin: ["http://localhost:3000", "https://ag-job-sheet.vercel.app"] })
 );
+
+const logger = morgan(
+  ":method :url :status :res[content-length] - :response-time ms"
+);
+app.use(logger);
 
 app.get("/", (request: Request, response: Response) => {
   response.send("Express + TypeScript Server");
@@ -52,14 +57,17 @@ interface IOptions {
   pageRanges?: string;
   path: string;
 }
+let browser: Browser | null = null;
 export const generatePDF = async ({ pageRanges, path }: IOptions) => {
-  let browser: Browser | null = null;
   try {
-    browser = await playwright.chromium.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-    });
+    if (!browser) {
+      browser = await playwright.chromium.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+      });
+    }
+
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(path, { waitUntil: "networkidle" });
@@ -77,7 +85,6 @@ export const generatePDF = async ({ pageRanges, path }: IOptions) => {
     return pdf;
   } catch (error) {
     console.log(error);
-  } finally {
     await browser?.close();
   }
 };
